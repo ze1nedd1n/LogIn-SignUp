c1cc9d1907b21545f75333eda7b70524071816fe
Zeineddin <101195142+ze1nedd1n@users.noreply.github.com>
2024-01-17 01:12:25 +0600

    сделал sweetalert, проверку пароля но таки не смог исправить pattern, второй бонус задачу при сущ email выводить уведомление

--- script/script.js
+++ script/script.js
@@ -44,8 +44,13 @@ function Login(){
         if(email_index == pass_index && email_index != -1){
             document.getElementsByClassName("wrong")[0].style.display = "none";
             document.getElementsByClassName("text_email")[0].style.marginTop = "10vh";
-            alert("Welcome, "+ users[email_index].name);
-            location="../index.html"
+
+            Swal.fire({
+                title: "Welcome, " + users[email_index].name,
+                icon: "success",
+              }).then(() => {
+                location.href = "../index.html";
+              });
         }
         else{
             console.log("no");  
@@ -94,42 +99,37 @@ function check_fields_login(){
 
 
 
-
 function SignUp(){
     
 
     if(check_fields_signUp()){
-        console.log("work");
         
         let name = document.getElementById("name").value;
-        let email = document.getElementById("email").value;
+        let Email = document.getElementById("email").value;
         let pass = document.getElementById("password").value;
         let age = document.getElementById("Age").value;
         let gender = document.querySelector('input[name="gender"]:checked').value;
 
+        const check_email = users.findIndex(({ email }) => email === document.getElementById("email").value);
+        if(check_email < 0){
+            let user = new Object();
+            user.name = name;
+            user.email = Email;
+            user.password = pass;
+            user.age = age;
+            user.gender = gender;
+
+            users.push(user);
+            console.log(users);
+        }
+        else{
+            Swal.fire({
+                title: "Аккаунт с такой почтой уже существует",
+                icon: "error",
+              });
+        }
+
         
-
-        let user = new Object();
-        user.name = name;
-        user.email = email;
-        user.password = pass;
-        user.age = age;
-        user.gender = gender;
-
-        users.push(user);
-        console.log(users);
-
-
-        // let users = JSON.stringify(user);
-        
-        // fetch('/users.json').then(res=> res.json()).then(res => {
-        //     let users = JSON.stringify(res);
-            
-        //     console.log(res);
-        // })
-        // i++;
-        // console.log(users);
-
 
     }
 
@@ -138,6 +138,63 @@ function SignUp(){
 //     event.preventDefault(); // Предотвращает отправку формы по умолчанию
 //     SignUp();
 // });
+
+let myInput = document.getElementById("password");
+let letter = document.getElementById("letter");
+let capital = document.getElementById("capital");
+let number = document.getElementById("number");
+let length = document.getElementById("length");
+// Когда пользователь нажимает на поле Пароль, отображается окно сообщения
+myInput.onfocus = function() {
+    document.getElementById("message").style.display = "block";
+  }
+  
+  // Когда пользователь щелкает за пределами поля пароля, скрыть окно сообщения
+  myInput.onblur = function() {
+    document.getElementById("message").style.display = "none";
+  }
+  
+  // Когда пользователь начинает вводить что-то в поле пароля
+  myInput.onkeyup = function() {
+    // Проверка строчных букв
+    let lowerCaseLetters = /[a-z]/g;
+    if(myInput.value.match(lowerCaseLetters)) {
+      letter.classList.remove("invalid");
+      letter.classList.add("valid");
+    } else {
+      letter.classList.remove("valid");
+      letter.classList.add("invalid");
+  }
+  
+    // Проверка заглавных букв
+    let upperCaseLetters = /[A-Z]/g;
+    if(myInput.value.match(upperCaseLetters)) {
+      capital.classList.remove("invalid");
+      capital.classList.add("valid");
+    } else {
+      capital.classList.remove("valid");
+      capital.classList.add("invalid");
+    }
+  
+    // Проверка чисел
+    let numbers = /[0-9]/g;
+    if(myInput.value.match(numbers)) {
+      number.classList.remove("invalid");
+      number.classList.add("valid");
+    } else {
+      number.classList.remove("valid");
+      number.classList.add("invalid");
+    }
+  
+    // Проверить длину
+    if(myInput.value.length >= 8) {
+      length.classList.remove("invalid");
+      length.classList.add("valid");
+    } else {
+      length.classList.remove("valid");
+      length.classList.add("invalid");
+    }
+  }
 
 
 function check_fields_signUp(){
